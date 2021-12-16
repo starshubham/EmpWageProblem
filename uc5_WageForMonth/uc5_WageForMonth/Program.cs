7a74a3d293f27f814e0139c942001a4be6611910
Shubham Seth <shubhamsth1234@gmail.com>
2021-12-17 00:11:26 +0530

    Added uc5 emp wage for month prob

---
+++ uc5_WageForMonth/uc5_WageForMonth/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+// Calculating Wages for a Month assuming 20 Working Days in a Month
+
+namespace uc5_WageForMonth
+{
+    class Program
+    {
+        public const int isFullTime = 1;
+        public const int isPartTime = 2;
+        public const int empRatePerHour = 20;
+        public const int numOfWorkingDays = 20;
+        static void Main(string[] args)
+        {
+            // Variables
+            int empHrs = 0;
+            int empWage = 0;
+            int totalEmpWage = 0;
+
+            // Computation
+            for(int day = 0; day <numOfWorkingDays; day++)
+            {
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+
+                switch (empCheck)
+                {
+                    case isFullTime:
+                        empHrs = 8;
+                        break;
+                    case isPartTime:
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empWage = empHrs * empRatePerHour;
+                totalEmpWage += empWage;
+                Console.WriteLine("Emp Wage: " + empWage);
+            }
+            Console.WriteLine("Total Emp Wage: " + totalEmpWage);
+        }
+    }
+}
