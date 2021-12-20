bfdd3d5cd0f7810ac231f9978a81e28269327f2f
Shubham Seth <shubhamsth1234@gmail.com>
2021-12-20 11:59:05 +0530

    Added Emp wage for multiple companies problem

---
+++ uc8_EmpWageForMultipleCompanies/uc8_EmpWageForMultipleCompanies/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+//Compute Employee Wage for Multiple Company in Procedural Way Using Class Methods
+
+namespace uc8_EmpWageForMultipleCompanies
+{
+    class Program
+    {
+        public const int isPartTime = 1;
+        public const int isFullTime = 2;
+
+        public static int computeEmpWage(string company, int empRatePerHour, 
+            int numOfWorkingDays, int maxHoursPerMonth)
+        {
+            //Variables
+            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+            //Computation
+            while (totalEmpHrs < maxHoursPerMonth && totalWorkingDays < numOfWorkingDays)
+            {
+                totalWorkingDays++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case isPartTime:
+                        empHrs = 4;
+                        break;
+                    case isFullTime:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Day#:" + totalWorkingDays + " Emp Hrs: " + empHrs);
+            }
+            int totalEmpWage = totalEmpHrs * empRatePerHour;
+            Console.WriteLine("Total Emp Hrs: " + totalEmpHrs);
+            Console.WriteLine("Total Emp Wage: " + totalEmpWage);
+            return totalEmpWage;
+        }
+        static void Main(string[] args)
+        {
+            computeEmpWage("DMart", 20, 2, 10);
+            computeEmpWage("Reliance", 10, 4, 20);
+        }
+    }
+}
