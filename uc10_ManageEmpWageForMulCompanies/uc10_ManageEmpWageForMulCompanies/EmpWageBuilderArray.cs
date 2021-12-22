a26ad0d0ea76c609d135853886cef4937cf3faee
Shubham Seth <shubhamsth1234@gmail.com>
2021-12-22 11:54:34 +0530

    Added Manage employee wage for multiple companies problem

---
+++ uc10_ManageEmpWageForMulCompanies/uc10_ManageEmpWageForMulCompanies/EmpWageBuilderArray.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uc10_ManageEmpWageForMulCompanies
+{
+    public class EmpWageBuilderArray
+    {
+        public const int isPartTime = 1;
+        public const int isFullTime = 2;
+
+        private int numOfCompany = 0;
+        private CompanyEmpWage[] companyEmpWageArray;
+
+        public EmpWageBuilderArray()
+        {
+            this.companyEmpWageArray = new CompanyEmpWage[5];
+        }
+
+        public void addCompanyEmpWage(string company, int empRatePerHour, int noOfWorkingDays, int maxHourPerMonth)
+        {
+            companyEmpWageArray[this.numOfCompany] = new CompanyEmpWage(company, empRatePerHour, noOfWorkingDays, maxHourPerMonth);
+            numOfCompany++;
+        }
+
+        public void computeEmpWage()
+        {
+            for(int i = 0; i < numOfCompany; i++)
+            {
+                companyEmpWageArray[i].SetTotalEmpWage(this.computeEmpWage(this.companyEmpWageArray[i]));
+                Console.WriteLine(this.companyEmpWageArray[i].toString());
+            }
+        }
+
+        private int computeEmpWage(CompanyEmpWage companyEmpWage)
+        {
+            //Variables
+            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+            //Computation
+            while (totalEmpHrs < companyEmpWage.maxHoursPerMonth && totalWorkingDays < companyEmpWage.numOfWorkingDays)
+            {
+                totalWorkingDays++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case isPartTime:
+                        empHrs = 4;
+                        break;
+                    case isFullTime:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Day#:" + totalWorkingDays + " Emp Hrs: " + empHrs);
+            }
+            return totalEmpHrs * companyEmpWage.empRatePerHour;
+        }
+    }
+}
---
+++ uc10_ManageEmpWageForMulCompanies/uc10_ManageEmpWageForMulCompanies/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+/*
+Ability to manage Employee Wage of multiple companies
+- Note: Refactor to have one EmpWageBuilder to manage for Wage for multiple Company
+- Create CompanyEmpWage class and let EmpWageBuilder has array of many CompanyEmpWage Object
+*/
+
+namespace uc10_ManageEmpWageForMulCompanies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***Welome to Employee Wage Program****");
+            Console.WriteLine("=====================================================================================================");
+
+            EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
+            empWageBuilder.addCompanyEmpWage("DMart", 20, 2, 10);
+            empWageBuilder.addCompanyEmpWage("Reliance", 10, 4, 20);
+            empWageBuilder.addCompanyEmpWage("TCS", 15, 3, 15);
+            empWageBuilder.addCompanyEmpWage("Samsung", 25, 5, 22);
+            empWageBuilder.addCompanyEmpWage("Facebook", 30, 6, 25);
+            empWageBuilder.computeEmpWage();
+        }
+    }
+}
