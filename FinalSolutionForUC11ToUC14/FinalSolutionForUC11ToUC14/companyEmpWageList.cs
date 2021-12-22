ace01d7fe651a33e9a89d1cd09f330928228706f
Shubham Seth <shubhamsth1234@gmail.com>
2021-12-22 23:10:49 +0530

    Added uc11 to uc14 problem

---
+++ FinalSolutionForUC11ToUC14/FinalSolutionForUC11ToUC14/companyEmpWageList.cs
@@ -0,0 +1,18 @@
+﻿namespace FinalSolutionForUC11ToUC14
+{
+    internal class companyEmpWageList
+    {
+        private string company;
+        private int empRatePerHour;
+        private int numOfWorkingDays;
+        private int maxHoursPerMonth;
+
+        public companyEmpWageList(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
+        {
+            this.company = company;
+            this.empRatePerHour = empRatePerHour;
+            this.numOfWorkingDays = numOfWorkingDays;
+            this.maxHoursPerMonth = maxHoursPerMonth;
+        }
+    }
+}
